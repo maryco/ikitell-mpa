bf2715d6871babf0a8916eb6272857b24e9acf83
maryco <maryco@users.noreply.github.com>
2019-04-05 15:52:18 +0900

    Update email subject.

--- resources/lang/en/email.php
+++ resources/lang/en/email.php
@@ -10,13 +10,21 @@ return [
     */
 
     'subject' => [
-        'verify_email' => '[Ikitell] Verify Email Address',
+        //'verify_email' => '[Ikitell] Verify Email Address',
         'reset_password' => '[Ikitell] Reset Password Notification',
+        'verify_email_register' => '[Ikitell] Verify Your Email Address',
+        'verify_email_contacts' => '[Ikitell] Please Verify This Email Address',
+        'contacts_email_verified' => '[Ikitell] Your Contacts Has Verified',
+        'device_resumed' => '[Ikitell] Your Device Has Resumed',
     ],
 
     'action' => [
-        'do_verify' => 'Verify Email Address',
+        //'do_verify' => 'Verify Email Address',
         'do_reset_password' => 'Reset Password',
+        'do_verify_register' => 'Verify This Email Address',
+        'do_verify_contacts' => 'Verify This Email Address',
+        'show_contacts' => 'View The Contacts',
+        'show_device' => 'View The Device',
     ],
 
     'unknown_name' => '(Unknown name)',
--- resources/lang/ja/email.php
+++ resources/lang/ja/email.php
@@ -10,13 +10,16 @@ return [
     */
 
     'subject' => [
+        //'verify_email' => '[Ikitell] Verify Email Address',
+        'reset_password' => '[Ikitell] パスワード再設定',
         'verify_email_register' => '[Ikitell] 登録メールアドレスの確認',
         'verify_email_contacts' => '[Ikitell] メールアドレス利用の承認依頼',
         'contacts_email_verified' => '[Ikitell] 通知先のメールアドレスが承認されました',
-        'device_resumed' => '[Ikitell] 休止設定期間が終了のお知らせ',
+        'device_resumed' => '[Ikitell] 端末の休止期間終了のお知らせ',
     ],
 
     'action' => [
+        //'do_verify' => 'Verify Email Address',
         'do_verify_register' => 'メールアドレスの登録を承認する',
         'do_reset_password' => 'パスワード再設定',
         'do_verify_contacts' => 'メールアドレスを承認する',
